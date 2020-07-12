2faf48c7e06ac1704ccf38eb4d3c4e55f21a8663
Phan Văn Trường <truongfct.040920@gmail.com>
2020-07-12 09:50:16 +0700

    Edit cookvalid.php

--- web_ttcnpm/cookvalid.php
+++ web_ttcnpm/cookvalid.php
@@ -1,31 +1,25 @@
 <?php
-header("location:home.php");
+header("location:cookHome.php");
 require("conection.php");
 
 $username = isset($_POST['uname']) ? $_POST['uname'] : '';
 $password = isset($_POST['psw']) ? $_POST['psw'] : '';
 
-
-
-$sql = "SELECT id, username, psw FROM user";
+$sql = "SELECT username, psw FROM cooks";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         if($row['username'] == $username && $row['psw'] == $password){
-            // Lưu Session
-            session_start();
-            $_SESSION['name'] = $row['username'];
-            $_SESSION['id'] = $row['id'];
             exit;
         }
     }
-    echo "<script> alert('wrong username and password, try again'); </script>";
-    header("location:index.php");
-   
+    echo "wrong username and password, try again";
+    header("location:cookLogin.php");
 }   else {
     echo "no usernam and password in database";
+    header("location:cookLogin.php");
 }
 mysqli_close($conn);
 ?>
