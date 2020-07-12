81f0b7eac2609271d0909bb70cb7bad1530eb3c2
Vix-Nguyen <59189226+Vix-Nguyen@users.noreply.github.com>
2020-07-12 21:01:40 +0700

    Update header.php

--- web_ttcnpm/header.php
+++ web_ttcnpm/header.php
@@ -11,7 +11,7 @@ else{
 
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="index.php" class="navbar-brand">
+        <a href="home.php" class="navbar-brand">
             <h3 class="px-5">
                 BK Food Court
             </h3>
