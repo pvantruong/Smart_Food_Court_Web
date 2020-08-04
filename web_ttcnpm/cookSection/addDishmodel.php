78e42de09ec48a027a6bea80695e8611be5d0b22
Vix-Nguyen <59189226+Vix-Nguyen@users.noreply.github.com>
2020-08-04 22:45:38 +0700

    Add dish now have an defdfault image

--- web_ttcnpm/cookSection/addDishmodel.php
+++ web_ttcnpm/cookSection/addDishmodel.php
@@ -6,10 +6,11 @@ require("conection.php");
 $name = isset($_POST['name']) ? $_POST['name'] : '';
 $vendor = isset($_POST['vendor']) ? $_POST['vendor'] : '';
 $price = isset($_POST['price']) ? $_POST['price'] : '';
+$image = "images/no-image.png";
 $avb = 0;
 
-$sql = "INSERT INTO menu (dishname, vendor, price, Avb)
-VALUES ('$name', '$vendor', '$price', '$avb')";
+$sql = "INSERT INTO menu (dishname, vendor, price, Avb, img)
+VALUES ('$name', '$vendor', '$price', '$avb', '$image')";
 
 $result = mysqli_query($conn, $sql);
 if ($result) {
